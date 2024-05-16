c08e30bcc353730bdc0b1c7016cd64fb470e5c39
Vullwen <celian.pinquier@gmail.com>
2024-05-16 08:07:03 +0000

    Revert "Revert "[FIX] The bot is now functionnal. It recive and send messages correctly.""
    This reverts commit 62fd2fa0a3ca0f773f72c623baa464694facadd5.

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\Drivers\DriverManager;
-use BotMan\BotMan\Messages\Incoming\Answer;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 class BotManController extends Controller
 {
@@ -14,15 +15,18 @@ class BotManController extends Controller
         if (app()->bound('debugbar')) {
             app('debugbar')->disable();
         }
-        //DriverManager::loadDriver(\BotMan\Drivers\Web\WebDriver::class);
 
-        $config = [];
-        $botman = app('botman');
+        $config = [
+            
+        ];
+        DriverManager::loadDriver(\BotMan\Drivers\Web\WebDriver::class);
+        $botman = BotManFactory::create($config);
 
-        $botman->hears('{message}', function ($botman, $message) { 
-            //renvoyer un json avec 'bonjour, "message"'
-            $botman->reply('Bonjour, '.$message);
-            return response()->json(['message' => $message]);
+
+        $botman->hears('{message}', function ($botman, $message) {
+            $message = request()->input('message');
+            $responseMessage = 'Bonjour, ' . $message;
+            $botman->reply($responseMessage);
         });
 
         $botman->listen();
